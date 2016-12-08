5149e799970aaab5b5d0ee8367e0d9189de096d5
estermer <eric.stermer@gmail.com>
2016-12-08 01:06:26 -0500

    finished styling for the home and about page
    edited branding statement
    
    added a photo I like
    
    included a fade in and fade out of about section on scroll and scroll distance from top

--- js/script.js
+++ js/script.js
@@ -1,10 +1,215 @@
 ///*ERICSTERMER.COM*///
 
+var railsProjects = [
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+];
+var meanProjects = [
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+];
+var jsProjects = [
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+];
+var challenges = [
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+];
+var contributions = [
+  {
+    title: "",
+    appUrl: "",
+    githubUrl: "",
+    description: "",
+    tech: "",
+  },
+];
+
 $(function(){
+
 
   ///NAV BUTTON CLICK FOR DROPDOWN MENU///
   $('#hamburger').on('click', function(){
     $('#dropdown-content').slideToggle();
   });
-
+  
+  ///NAV SCROLL FADE IN ABOUT WHEN REACH ABOUT///
+  $(window).on('scroll', function(){
+    var top = $(window).scrollTop();
+    var width = $(window).width();
+    if(width > 950) {
+      if(top > 440 && top < 1170){  
+        $('#container').fadeIn('slow');
+      } else {
+        $('#container').fadeOut('slow');
+      }
+    } else {
+      if(top > 440 && top < 1850){  
+        $('#container').fadeIn('slow');
+      } else {
+        $('#container').fadeOut('slow');
+      }
+    }
+  });
 });
