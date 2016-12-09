47d50ef7f01e76fec3d8d1278aacc2f833a39f1b
estermer <eric.stermer@gmail.com>
2016-12-09 16:22:27 -0500

    edited main site

---
+++ js/project-data.js
@@ -0,0 +1,161 @@
+var railsProjects = [
+  {
+    title: "NITPIC",
+    img: "./img/NITPIC.png",
+    appUrl: "https://nitpic-app-ui.herokuapp.com/#/",
+    githubUrl: "https://github.com/estermer/nitpic-ui",
+    description: "A photo sharing application.",
+    tech: "Ruby on Rails, Angular, HTML, CSS, AWS S3, User Auth",
+  },
+  {
+    title: "Pollster",
+    img: "./img/pollster.jpg",
+    appUrl: "https://stermer-pollster-app.herokuapp.com/",
+    githubUrl: "https://github.com/estermer/pollster-ui",
+    description: "An app to create and participate in polls.",
+    tech: "Ruby on Rails, Angular, HTML, CSS",
+  }
+];
+
+var meanProjects = [
+  {
+    title: "Shop Mate",
+    img: "./img/shopmate.png",
+    appUrl: "http://shopmate-app.herokuapp.com/#/",
+    githubUrl: "https://github.com/estermer/grocery-list-app",
+    description: "Track your shopping list for your trip to the grocery store.",
+    tech: "MEAN Stack, Flikr API",
+  },
+  {
+    title: "Mile Logger",
+    img: "./img/milelogger.jpg",
+    appUrl: "http://milelogger.herokuapp.com/",
+    githubUrl: "https://github.com/estermer/mile-logger",
+    description: "Track and log your miles you run.",
+    tech: "Node, Express, Server Side Rendering",
+  },
+  {
+    title: "Pokedex",
+    img: "./img/pokedex.jpeg",
+    appUrl: "https://ericpokedexserver.herokuapp.com/",
+    githubUrl: "https://github.com/estermer/eric-pokedex-server",
+    description: "",
+    tech: "Node, Express, Handlebars",
+  }
+];
+
+var jsProjects = [
+  {
+    title: "Black Jack",
+    img: "./img/blackjack.jpg",
+    appUrl: "http://www.ericstermer.com/projects/blackjack.html",
+    githubUrl: "https://github.com/estermer/BlackJack-GA",
+    description: "A simple game of Black Jack.",
+    tech: "HTML, CSS3, jQuery, OOP",
+  },
+  {
+    title: "Liberty Notepad Generator",
+    img: "./img/libertygen.jpg",
+    appUrl: "http://www.ericstermer.com/projects/libertygen.html",
+    githubUrl: "",
+    description: "",
+    tech: "HTML, CSS, jQuery",
+  },
+  {
+    title: "Stop Watch",
+    img: "./img/stopwatch.jpeg",
+    appUrl: "http://www.ericstermer.com/projects/stopwatch.html",
+    githubUrl: "",
+    description: "",
+    tech: "HTML, CSS, jQuery",
+  },
+  {
+    title: "Pomodoro Clock",
+    img: "./img/pomodoro.jpeg",
+    appUrl: "http://www.ericstermer.com/projects/pomodoro.html",
+    githubUrl: "",
+    description: "",
+    tech: "HTML, CSS, jQuery",
+  },
+  {
+    title: "Calculator",
+    img: "./img/calc.jpg",
+    appUrl: "http://www.ericstermer.com/projects/calc.html",
+    githubUrl: "",
+    description: "",
+    tech: "HTML, CSS, jQuery",
+  },
+  {
+    title: "Weather App",
+    img: "./img/weatherApp.jpg",
+    appUrl: "http://www.ericstermer.com/projects/weatherApp.html",
+    githubUrl: "",
+    description: "",
+    tech: "HTML, CSS, AJAX, jQuery",
+  }
+];
+
+var challenges = [
+  {
+    title: "FreeCodeCamp",
+    img: "./img/freecodecamp.png",
+    appUrl: "https://www.freecodecamp.com/estermer",
+    description: "",
+    tech: "HTML, CSS, JavaScript, AJAX",
+  },
+  {
+    title: "Codepen",
+    img: "./img/codepen.png",
+    appUrl: "http://codepen.io/estermer/",
+    description: "",
+    tech: "HTML, CSS, JavaScript",
+  },
+  {
+    title: "Khan Academy",
+    img: "./img/khanacademy.png",
+    appUrl: "https://www.khanacademy.org/profile/kaid_1090611702257418820503017/projects",
+    description: "",
+    tech: "HTML, CSS, JavaScript",
+  },
+  {
+    title: "Codewars",
+    img: "./img/codewars.jpg",
+    appUrl: "https://www.codewars.com/users/estermer",
+    description: "",
+    tech: "JavaScript and Ruby",
+  },
+  {
+    title: "Hacker Rank",
+    img: "./img/hackerrank.jpg",
+    appUrl: "https://www.hackerrank.com/eric_stermer",
+    description: "",
+    tech: "Ruby",
+  }
+];
+
+var contributions = [
+  {
+    title: "If Me",
+    img: "./img/ifme.jpg",
+    appUrl: "https://www.if-me.org/",
+    githubUrl: "https://github.com/estermer/ifme",
+    description: "Open Source Contributor to help provide a place for support for those who suffer from mental illness",
+    tech: "Rails",
+  },
+  {
+    title: "Add-me",
+    img: "./img/qrpic.png",
+    appUrl: "https://socialaddme.herokuapp.com/",
+    githubUrl: "https://github.com/JonathanGrant/AddMeForReal",
+    description: "Big Red Hack-a-thon best business idea at Cornell University, Fall 2016.",
+    tech: "Node.JS, Oauth, AWS",
+  },
+  {
+    title: "RIPTA Reviews",
+    img: "./img/RIPTA_Logo.png",
+    appUrl: "https://danman01.github.io/ripta-reviews/",
+    githubUrl: "https://github.com/estermer/ripta-reviews",
+    description: "HackRI 2016 submission for REV RIPTA competition.",
+    tech: "Rails, Angular, Google Maps API",
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,215 +1,99 @@
 ///*ERICSTERMER.COM*///
 
-var railsProjects = [
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-];
-var meanProjects = [
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-];
-var jsProjects = [
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-];
-var challenges = [
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-];
-var contributions = [
-  {
-    title: "",
-    appUrl: "",
-    githubUrl: "",
-    description: "",
-    tech: "",
-  },
-];
-
 $(function(){
-
 
   ///NAV BUTTON CLICK FOR DROPDOWN MENU///
   $('#hamburger').on('click', function(){
     $('#dropdown-content').slideToggle();
   });
-  
+
   ///NAV SCROLL FADE IN ABOUT WHEN REACH ABOUT///
   $(window).on('scroll', function(){
     var top = $(window).scrollTop();
     var width = $(window).width();
+
+    //FADE IN/OUT ABOUT ME
     if(width > 950) {
-      if(top > 440 && top < 1170){  
+      if(top > 440 && top < 1170){
         $('#container').fadeIn('slow');
       } else {
         $('#container').fadeOut('slow');
       }
     } else {
-      if(top > 440 && top < 1850){  
+      if(top > 440 && top < 1950){
         $('#container').fadeIn('slow');
       } else {
         $('#container').fadeOut('slow');
       }
     }
+
+    //FADE PROJECTS IN/OUT
+    if(width > 950) {
+      if(top > 1170){
+        $('#btns').fadeIn('slow');
+        $('#thumbs').fadeIn('slow');
+        $('#projects-title').fadeIn('slow');
+      } else {
+        $('#btns').fadeOut('slow');
+        $('#thumbs').fadeOut('slow');
+        $('#projects-title').fadeOut('slow');
+      }
+    } else {
+      if(top > 2050){
+        $('#btns').fadeIn('slow');
+        $('#thumbs').fadeIn('slow');
+        $('#projects-title').fadeIn('slow');
+      } else {
+        $('#btns').fadeOut('slow');
+        $('#thumbs').fadeOut('slow');
+        $('#projects-title').fadeOut('slow');
+      }
+    }
   });
+
+  $('#rails').on('click', function(){
+    showProjects(railsProjects);
+  });
+
+  $('#mean').on('click', function(){
+    showProjects(meanProjects);
+  });
+
+  $('#js').on('click', function(){
+    showProjects(jsProjects);
+  });
+
+  $('#collab').on('click', function(){
+    showProjects(contributions);
+  });
+
+  $('#school').on('click', function(){
+    showProjects(challenges);
+  });
+
+  function showProjects(list) {
+    $('#thumbs').html('');
+
+    for(var i = 0; i < list.length; i ++){
+      var $thumb = $('div');
+      $thumb.addClass('project-thumb');
+      $thumb.html(`
+        <h3>${list[i].title}</h3>
+        <img src="${list[i].img}" alt="${list[i].title}" />
+        <p>
+          ${list[i].description}
+        </p>
+        <h4>
+          <a class="fa fa-play-circle" href="${list[i].appUrl}"></a>
+          <a class="fa fa-github" href="${list[i].githubUrl}"></a>
+        </h4>
+      `);
+      $('#thumbs').append($thumb);
+    }
+
+    var setOfThree = list.length%3;
+
+  };
+
+
 });
