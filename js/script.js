83d87fc3a99a6bcfe62b3d8d97df5b1ef7a1cb6a
estermer <eric.stermer@gmail.com>
2017-06-24 10:53:07 -0400

    minified the js and jquery for homepage to portfolio

--- js/script.js
+++ js/script.js
@@ -9,50 +9,6 @@ $(function(){
     $('#dropdown-content').slideToggle();
   });
 
-  ///NAV SCROLL FADE IN ABOUT WHEN REACH ABOUT///
-  // $(window).on('scroll', function(){
-  //   var top = $(window).scrollTop();
-  //   var width = $(window).width();
-  //
-  //   //FADE IN/OUT ABOUT ME
-  //   if(width > 991) {
-  //     if(top > 440 && top < 1200){
-  //       $('.about').fadeIn('slow');
-  //     } else {
-  //       $('.about').fadeOut('slow');
-  //     }
-  //   } else if (width < 992 && width > 575) {
-  //     if(top > 440 && top < 1750){
-  //       $('.about').fadeIn('slow');
-  //     } else {
-  //       $('.about').fadeOut('slow');
-  //     }
-  //   }
-  //
-  //   //FADE PROJECTS IN/OUT
-  //   if(width > 991) {
-  //     if(top > 1200){
-  //       $('#btns').fadeIn('slow');
-  //       $('#thumbs').fadeIn('slow');
-  //       $('#projects-title').fadeIn('slow');
-  //     } else {
-  //       $('#btns').fadeOut('slow');
-  //       $('#thumbs').fadeOut('slow');
-  //       $('#projects-title').fadeOut('slow');
-  //     }
-  //   } else if (width < 992 && width > 575) {
-  //     if(top > 1800){
-  //       $('#btns').fadeIn('slow');
-  //       $('#thumbs').fadeIn('slow');
-  //       $('#projects-title').fadeIn('slow');
-  //     } else {
-  //       $('#btns').fadeOut('slow');
-  //       $('#thumbs').fadeOut('slow');
-  //       $('#projects-title').fadeOut('slow');
-  //     }
-  //   } else {}
-  // });
-
   $('#rails').on('click', function(){
     showProjects(railsProjects, $projectOriginalHeight);
   });
@@ -81,19 +37,17 @@ $(function(){
       // $thumb.addClass('col-xs-6');
       $thumb.addClass('col-md-6');
       $thumb.addClass('col-lg-4');
-      $thumb.html(`
-        <div class="project-thumb">
-          <h3>${list[i].title}</h3>
-          <img src="${list[i].img}" alt="${list[i].title}" />
-          <p>
-            ${list[i].description}
-          </p>
-          <h4>
-            <a class="fa fa-play-circle" href="${list[i].appUrl}" target="_blank"></a>
-            <a class="fa fa-github" href="${list[i].githubUrl}" target="_blank"></a>
-          </h4>
-        </div>
-      `);
+      $thumb.html('<div class="project-thumb">' +
+          '<h3>' + list[i].title + '</h3>' +
+          '<img src="' + list[i].img + '" alt="' + list[i].title + '" />' +
+          '<p>' +
+            list[i].description +
+          '</p>' +
+          '<h4>' +
+            '<a class="fa fa-play-circle" href="' + list[i].appUrl + '" target="_blank"></a>' +
+            '<a class="fa fa-github" href="' + list[i].githubUrl + '" target="_blank"></a>' +
+          '</h4>' +
+        '</div>');
       console.log($thumb);
       $('#thumbs').append($thumb);
     }
@@ -116,6 +70,5 @@ $(function(){
       $('#projects').height(height);
     }
   };
-
 
 });
